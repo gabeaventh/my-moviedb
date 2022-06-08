5a2867cdb385fcb7bb47e7c5008c2e5401869812
Thomas Dwiatmoko <gabeaventh@gmail.com>
2022-06-08 23:30:00 +0700

    remove unused skeleton files

--- lib/modules/authentication/bloc/auth_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'auth_bloc.dart';
-
-@freezed
-class AuthEvent with _$AuthEvent {
-  const factory AuthEvent.signin() = AuthSignIn;
-  const factory AuthEvent.signout() = AuthSignOut;
-  const factory AuthEvent.authStatus() = AuthStatus;
-}
--- lib/modules/home/section/stocks/cubit/fav/stock_favorite_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'stock_favorite_cubit.dart';
-
-@freezed
-class StockFavoriteState with _$StockFavoriteState {
-  const factory StockFavoriteState.initial() = StockFavoriteInitial;
-  const factory StockFavoriteState.success(List<String> fav) =
-      StockFavoriteSuccess;
-  const factory StockFavoriteState.loading() = StockFavoriteLoading;
-}
