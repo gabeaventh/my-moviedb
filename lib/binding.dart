47bea5a998b4734c46c5cf61d3ad52c580c07f43
Thomas Dwiatmoko <gabeaventh@gmail.com>
2022-06-08 23:33:30 +0700

    add app binding and app routes

--- lib/binding.dart
+++ lib/binding.dart
@@ -1,10 +1,13 @@
 import 'package:get/get.dart';
 import 'package:moviedb/modules/home/cubit/home_tab_cubit.dart';
-import 'package:moviedb/modules/home/section/watch_list/cubit/watch_list_cubit.dart';
 import 'package:moviedb/modules/movie/cubit/movie_cubit.dart';
 import 'package:moviedb/modules/movie/service/service.dart';
 import 'package:moviedb/modules/movie_details/cubit/movie_details_cubit.dart';
 import 'package:moviedb/modules/movie_details/service/service.dart';
+import 'package:moviedb/modules/television/cubit/television_cubit.dart';
+import 'package:moviedb/modules/television/service/service.dart';
+import 'package:moviedb/modules/television_details/cubit/television_details_cubit.dart';
+import 'package:moviedb/modules/television_details/service/service.dart';
 
 /// These are [GetX] binding that provides dependencies injection
 /// for the application to use.
@@ -12,10 +15,13 @@ class AppBinding implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut<HomeTabCubit>(HomeTabCubit.new);
-    Get.lazyPut<WatchListCubit>(WatchListCubit.new);
     Get.lazyPut<MovieService>(MovieService.new);
     Get.lazyPut<MovieCubit>(MovieCubit.new);
     Get.lazyPut<MovieDetailsService>(MovieDetailsService.new);
     Get.lazyPut<MovieDetailsCubit>(MovieDetailsCubit.new);
+    Get.lazyPut<TelevisionService>(TelevisionService.new);
+    Get.lazyPut<TelevisionCubit>(TelevisionCubit.new);
+    Get.lazyPut<TelevisionDetailsService>(TelevisionDetailsService.new);
+    Get.lazyPut<TelevisionDetailsCubit>(TelevisionDetailsCubit.new);
   }
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 import 'package:moviedb/binding.dart';
 import 'package:moviedb/modules/home/page.dart';
 import 'package:moviedb/modules/movie_details/view.dart';
+import 'package:moviedb/modules/profile/view.dart';
+import 'package:moviedb/modules/television_details/view.dart';
 import 'package:moviedb/routes/routes_name.dart';
 
 /// Initialize the Routes of the Application
@@ -21,6 +23,16 @@ class AppRoutes {
     GetPage(
       name: Routes.MOVIE_DETAILS,
       page: MovieDetailsView.new,
+      binding: AppBinding(),
+    ),
+    GetPage(
+      name: Routes.TV_DETAILS,
+      page: TelevisionDetailsView.new,
+      binding: AppBinding(),
+    ),
+    GetPage(
+      name: Routes.PROFILE,
+      page: ProfileView.new,
       binding: AppBinding(),
     ),
   ];
