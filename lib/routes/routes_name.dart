7cda029930eb81dda7ce77f1385182bd9c0400c8
Thomas Dwiatmoko <gabeaventh@gmail.com>
2022-06-08 23:31:24 +0700

    add routes name

--- lib/routes/routes_name.dart
+++ lib/routes/routes_name.dart
@@ -1,12 +1,12 @@
 abstract class Routes {
-  static const String ACCOUNT = APP + _Paths.ACCOUNT;
+  static const String PROFILE = APP + _Paths.PROFILE;
   static const APP = _Paths.APP;
   static const MOVIE_DETAILS = APP + _Paths.MOVIE + _Paths.DETAILS;
   static const TV_DETAILS = APP + _Paths.TV + _Paths.DETAILS;
 }
 
 abstract class _Paths {
-  static const ACCOUNT = '/account';
+  static const PROFILE = '/profile';
   static const APP = '/';
   static const DETAILS = '/details';
   static const MOVIE = 'movie';
